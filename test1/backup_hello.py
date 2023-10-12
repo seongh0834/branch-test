d15d367bb43165487bee712d60f396c9741c3771
Seonghak Song <seongh0834@gachon.ac.kr>
2023-10-12 20:42:00 +0900

    First commit

---
+++ test1/backup_hello.py
@@ -0,0 +1,30 @@
+#untitled
+
+
+print("Hello World!" * 3)
+print(1234+5678)
+
+'''
+this is commentory part
+
+
+
+'''
+
+x = 10
+
+if (x > 0):
+    print('the X is positive number')
+else:
+    print('the X is negative number')
+    
+
+'''
+
+Exercise lecture K01
+
+'''
+
+print(3.141592 * 10.0 * 128.0)
+print((1/100) * 1234)
+print((123456789 * 123456789) - 123456789)
